495ac886a0da41b76d25805c328777496aa9104b
nrysk <ryosuke.10117@gmail.com>
2025-08-17 14:00:15 +0900

    feat[frontend]: robots及びsitemapを生成するプラグインの導入

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,21 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import sitemapPlugin from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		sitemapPlugin({
+			hostname: "https://it-word-wolf.nrysk.dev",
+			robots: [
+				{
+					userAgent: "*",
+					allow: "/",
+				},
+			],
+		}),
+	],
 });
